42f4ffbcdd032ad2fd704f9b326319fdc4dcd314
overmind777 <63595243+overmind777@users.noreply.github.com>
2024-03-21 12:57:23 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html'
+    }
   },
 })
