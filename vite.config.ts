7d905a650da4561c5d900de94141c08bb7fca25f
overmind777 <63595243+overmind777@users.noreply.github.com>
2024-03-21 14:12:19 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    manifest: true,
-    outDir: './', // Change this to the current directory
+    outDir: 'dist',
   },
-  base:  '/',
-  root: './src',
   plugins: [react()],
 })
