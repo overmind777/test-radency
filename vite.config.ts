78b77b8f711921249e5b4def9426dd6622f338a6
overmind777 <63595243+overmind777@users.noreply.github.com>
2024-03-21 15:29:20 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: 'test-radency',
-  root: 'src',
+  root: '../src',
   build: {
-    outDir: 'dist'
+    outDir: '../dist'
   }
 })
